ce5855f8b92672fef22b0031cfb6c03cc0c70804
Paul Holsters <paul.holsters@hotmail.com>
2023-05-08 10:37:07 +0200

    fix bug getComponentThroughAttributes method

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,6 +4,8 @@ import {ConceptModel} from "./models/Data/ConceptModel";
 import {ConceptConfigModel} from "./models/Data/ConceptConfigModel";
 import {Apollo, gql} from "apollo-angular";
 import {AttributeModel} from "./models/Data/AttributeModel";
+import {Observable} from "rxjs";
+import {ApolloQueryResult} from "@apollo/client";
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +15,15 @@ export class DataService {
     // deze methode verzendt de data naar de componenten voor dewelke de
     // data is gewijzigd
     // todo hou rekening met de dataPipe
-
+    console.log(data,compName,compDataConfig)
   }
   constructor(private storeService: StoreService, private apollo: Apollo) {
   }
   // todo een taal bedenken voor extra calculated fields based on related data and concepts
   // todo a way to filter data
   // todo a way to order data
-  private fakeQuery(data: ConceptConfigModel): ConceptModel|undefined {
-    const productData = this.apollo
+  private fakeQuery(data: ConceptConfigModel): Observable<ApolloQueryResult<unknown>> {
+    return this.apollo
       .watchQuery({
         query: gql`
                     {
@@ -31,18 +33,6 @@ export class DataService {
                     }
         `,
       }).valueChanges
-      .subscribe(res => {
-      return res.data
-      })
-    if (productData instanceof Array) {
-      const [k, v] = Object.entries(productData[0])[0]
-      if (typeof v === 'string') {
-        const conceptName = productData[0].__typename
-        const attr = [new AttributeModel(k, v)]
-        return new ConceptModel(conceptName, attr)
-      }
-    } else return undefined
-    return undefined
   }
   private fakeMutation(data: ConceptModel) {
 
@@ -51,19 +41,22 @@ export class DataService {
     this.fakeMutation(data)
   }
   public componentReady(name: string) {
-    console.log('ok')
-    console.log(name)
     let componentConfig = this.storeService.getComponent(name)
     if (!componentConfig) {
       componentConfig = this.storeService.getComponentThroughAttributes(name)
     }
     if (componentConfig && componentConfig.data) {
-      console.log(componentConfig)
-      debugger
-      const result = this.fakeQuery(componentConfig.data)
-      console.log(result)
-      if (result)
-        this.setDataState(result, name, componentConfig.data)
+      this.fakeQuery(componentConfig.data).subscribe(res=>{
+        const productData = res.data
+        if (productData instanceof Array && componentConfig && componentConfig.data) {
+          const [k, v] = Object.entries(productData[0])[0]
+          if (typeof v === 'string') {
+            const conceptName = productData[0].__typename
+            const attr = [new AttributeModel(k, v)]
+            this.setDataState(new ConceptModel(conceptName, attr), name, componentConfig.data)
+          }
+        }
+      })
     }
   }
 }
