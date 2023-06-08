148aaedb676c79c7280a56aa9a09dc69042c4cb8
paulholsters <paul.holsters@hotmail.com>
2023-06-08 12:56:05 +0200

    voorbereiding form multiselect

---
+++ src/app/configuration/form2/formContainer2.ts
@@ -0,0 +1,47 @@
+import {ComponentModel} from "../../models/ComponentModel";
+import {ComponentType} from "../../enums/componentTypes.enum";
+import {formLayout2} from "./formLayout2";
+import {ResponsiveDimensioningConfigModel} from "../../models/Dimensioning/self/ResponsiveDimensioningConfigModel";
+import {DimensioningConfigPropsModel} from "../../models/Dimensioning/self/DimensioningConfigPropsModel";
+import {HeightConfigPropsModel} from "../../models/Dimensioning/self/HeightConfigPropsModel";
+import {FixedDimensioningConfigModel} from "../../models/Dimensioning/self/FixedDimensioningConfigModel";
+import {DimensionValueConfigType} from "../../enums/dimensionValueConfigTypes.enum";
+import {DimensionUnitConfigType} from "../../enums/dimensionUnitConfigTypes.enum";
+import {DynamicDimensionValueConfigType} from "../../enums/DynamicDimensionValueConfigTypes.enum";
+import {WidthConfigPropsModel} from "../../models/Dimensioning/self/WidthConfigPropsModel";
+import {ResponsiveVisibilityConfigModel} from "../../models/Visibility/ResponsiveVisibilityConfigModel";
+import {multiSelect} from "./multiSelect";
+import {ResponsiveAttributesConfigModel} from "../../models/Attributes/ResponsiveAttributesConfigModel";
+import {VisibilityConfigPropsModel} from "../../models/Visibility/VisibilityConfigPropsModel";
+import {formData2} from "./formDataModel2";
+
+export const formContainer2 = new ComponentModel(
+  'form-container',
+  ComponentType.Container,
+  formLayout2,
+  undefined,
+  new ResponsiveDimensioningConfigModel(new DimensioningConfigPropsModel(
+    new HeightConfigPropsModel(
+      new FixedDimensioningConfigModel(DimensionValueConfigType.Hardcoded, 100, DimensionUnitConfigType.Percentage), DynamicDimensionValueConfigType.NC
+    ),
+    new WidthConfigPropsModel(new FixedDimensioningConfigModel(
+      DimensionValueConfigType.Hardcoded, 100, DimensionUnitConfigType.Percentage
+    ), DynamicDimensionValueConfigType.NC)
+  )),
+  undefined,
+  new ResponsiveVisibilityConfigModel(new VisibilityConfigPropsModel()),
+  undefined,
+  [
+    multiSelect,
+    {
+      name:'submitbtn',
+      type:ComponentType.Button,
+      attributes: new ResponsiveAttributesConfigModel({
+        label:'Bewaar'
+      }),
+      visibility: new ResponsiveVisibilityConfigModel(new VisibilityConfigPropsModel())
+    }
+  ],
+  undefined,
+  formData2
+)
