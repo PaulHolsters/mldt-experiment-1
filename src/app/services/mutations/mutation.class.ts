51d185614eda80a495be4ed6c8491922c344f2da
paulholsters <paul.holsters@hotmail.com>
2023-08-16 17:06:10 +0200

    refactoring mutation service creating getStr method

--- src/app/services/mutations/mutation.class.ts
+++ src/app/services/mutations/mutation.class.ts
@@ -1,9 +1,7 @@
 import {MutationType} from "../../enums/mutationTypes.enum";
 import {ClientDataRenderModel} from "../../models/Data/ClientDataRenderModel";
 import {DataRecordModel} from "../../models/DataRecordModel";
-import {ClientDataConfigModel} from "../../models/Data/ClientDataConfigModel";
 import utilFunctions from "../../utils/utilFunctions";
-import {AttributeConfigModel} from "../../models/Data/AttributeConfigModel";
 import {NoValueType} from "../../enums/no_value_type";
 export class Mutation {
   public constructor(
@@ -11,7 +9,7 @@ export class Mutation {
     public readonly data:ClientDataRenderModel
   ) {
   }
-  public getData():DataRecordModel|(DataRecordModel|null)[]|Error{
+  private getData():DataRecordModel|(DataRecordModel|null)[]|Error{
     switch (this.type){
       case MutationType.Create:
         return this.data.record ?? new Error('data is undefined')
@@ -29,25 +27,44 @@ export class Mutation {
         return new Error('Mutationtype '+this.type+ ' does not exist')
     }
   }
-  private getMutationParams(data: AttributeConfigModel[] | NoValueType.DBI): string {
-    // todo refactor: get rid of conditionals => factory pattern!
-    if (data === NoValueType.DBI) return ''
-    const strVal = data.map(x => {
-        return `\
+  private getParamsForRecord(data:DataRecordModel|null):string{
+    return Object.entries(data).reduce(())
+  }
+
+  // todo refactor: get rid of conditionals => factory pattern!
+/*  if (data === NoValueType.DBI) return ''
+  const strVal = data.map(x => {
+      return `\
 ${(x.number?.value || x.text?.value || x.radio?.value || x.multiselect?.selectedOptions) ? (x.name + ':' || '') : ''}\
 ${(x.text?.value) ? '"' : (x.multiselect?.selectedOptions) ? '[' : ''}${(x.multiselect?.selectedOptions?.length ?? 0) > 0 ? '"' : ''}\
 ${(x.number?.value || x.text?.value || x.radio?.value || x.multiselect?.selectedOptions?.map(opt => {
-          return opt.id
-        }).join('","')) || ''}${(x.multiselect?.selectedOptions?.length ?? 0) > 0 ? '"' : ''}\
+        return opt.id
+      }).join('","')) || ''}${(x.multiselect?.selectedOptions?.length ?? 0) > 0 ? '"' : ''}\
 ${(x.text?.value) ? '"' : (x.multiselect?.selectedOptions) ? ']' : ''}
 `
-      }
-    )
-      .reduce((x, y) => x += `,` + y).trim()
-    // todo zorg nog voor een meer ordelijke GQL string hier
-    return strVal.charAt(strVal.length - 1) === ',' ? strVal.substring(0, strVal.length - 1) : strVal
+    }
+  )
+    .reduce((x, y) => x += `,` + y).trim()
+  // todo zorg nog voor een meer ordelijke GQL string hier
+  return strVal.charAt(strVal.length - 1) === ',' ? strVal.substring(0, strVal.length - 1) : strVal*/
+
+  private getParams(): string {
+    const data = this.getData()
+    if(data instanceof Array){
+      return data.map((record)=>{
+        return this.getParamsForRecord(record)
+      }).reduce((a,b)=>(a+','+b))
+    } else{
+      if(data instanceof Error) throw data
+      return this.getParamsForRecord(data)
+    }
+  }
+  private getReturnValues():string{
+    return ''
   }
   public getStr(): string {
+    // todo vervang het plus s gebeuren door ervan uit te gaan dat je kan gaan voor plural en singular values van de backend
+    //      maw doe dit van zodra je plural stuff wil testen
     /*
     * de voorwaarde voor het aanspreken van deze mutatie dus wanneer je een mutation action wil doen
     * is dat er voor deze actie een juist clientDataInstance bestaat zodat je data parameter inderdaad kqn geven alsook de conceptnaam
@@ -55,10 +72,16 @@ ${(x.text?.value) ? '"' : (x.multiselect?.selectedOptions) ? ']' : ''}
     * VERB = Type of Action
     * ConceptName = BackendMutation
     * PARAMS = DATARECORDMODEL
-    * return values = errorhandling + errormessages
+    * return values = errorhandling + errormessages = clientDataRenderModel.errorMessages
     * */
-
-
+    return `
+    mutation Mutation{
+      ${this.type}${utilFunctions.capitalizeFirst(this.data.conceptName)}(${this.getParams()}){
+        ${this.getReturnValues()}
+      }
+    }
+    `
+/*
     if (data instanceof ClientDataConfigModel) {
       const currentData = this.objectData.find(dataObj => {
         return dataObj.conceptName === data.conceptName
@@ -81,11 +104,6 @@ ${(x.text?.value) ? '"' : (x.multiselect?.selectedOptions) ? ']' : ''}
               }
             }`
     }
-    throw new Error('Geen geldige data configuratie.')
-    // todo maar naast een create single zou je ook een create multiple kunnen hebben en zelfs een update multiple
-    // todo en een delete zie ik hier precies niet dus als die er is is dt absoluut niet duidelijk en expliciet (genoeg)
-    //      of liever de verb bepaald diet maar daarna is er een hoop onduidelijke mumbojumbo met parameters en methodes
-    //      waarbij het niet duidelijk is wat dit doet en wat de relatie met de VERB parameter is
-    //
+    throw new Error('Geen geldige data configuratie.')*/
   }
 }
