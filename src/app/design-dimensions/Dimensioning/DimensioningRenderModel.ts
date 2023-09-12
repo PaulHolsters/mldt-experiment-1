369b6398b6e05da4ec9f06b4b6b94a2aee547fb4
paulholsters <paul.holsters@hotmail.com>
2023-09-12 17:05:24 +0200

    aanpassen rendermodellen

--- src/app/design-dimensions/Dimensioning/DimensioningRenderModel.ts
+++ src/app/design-dimensions/Dimensioning/DimensioningRenderModel.ts
@@ -1,14 +1,12 @@
 import {ComponentDimensionValueConfigType} from "../../enums/componentDimensionValueConfigTypes.enum";
 
 export class DimensioningRenderModel{
-  constructor(public height?: string | undefined | ComponentDimensionValueConfigType.Parent,
-              public width?: string | undefined | ComponentDimensionValueConfigType.Parent,
-              public grow?: number | undefined | ComponentDimensionValueConfigType.Parent, // main axis
-              public shrink?: number | undefined | ComponentDimensionValueConfigType.Parent, // main axis
-              public alignSelfStretch?: boolean | undefined, // cross axis
-              public fitContentHeight?: boolean |undefined,
-              public fitContentWidth?: boolean |undefined,
-              public calcHeight?: string | undefined | ComponentDimensionValueConfigType.Parent,
-              public calcWidth?: string | undefined| ComponentDimensionValueConfigType.Parent) {
-  }
+  public height: string | undefined | ComponentDimensionValueConfigType.Parent=undefined
+  public width: string | undefined | ComponentDimensionValueConfigType.Parent=undefined
+  public calcHeight: string | undefined | ComponentDimensionValueConfigType.Parent=undefined
+  public calcWidth: string | undefined| ComponentDimensionValueConfigType.Parent=undefined
+  public grow: number | undefined | ComponentDimensionValueConfigType.Parent=undefined // main axis
+  public shrink: number | undefined | ComponentDimensionValueConfigType.Parent=undefined // main axis
+  constructor() {}
+
 }
