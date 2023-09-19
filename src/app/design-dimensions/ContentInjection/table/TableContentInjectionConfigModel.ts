95965f2e62d9e587d7d8ff605411160737a9e193
paulholsters <paul.holsters@hotmail.com>
2023-09-19 15:53:16 +0200

    afwerken contentInjectmodels

--- src/app/design-dimensions/ContentInjection/table/TableContentInjectionConfigModel.ts
+++ src/app/design-dimensions/ContentInjection/table/TableContentInjectionConfigModel.ts
@@ -1,11 +1,26 @@
 import {ComponentModel} from "../../ComponentModel";
 import {ZeroValueType} from "../../../enums/zeroValueTypes.enum";
-
 export class TableContentInjectionConfigModel {
   public columnHeaderComponents: ComponentModel[]|ZeroValueType.NotConfigured=ZeroValueType.NotConfigured
   public footer: ComponentModel|ZeroValueType.NotConfigured=ZeroValueType.NotConfigured
   public caption: ComponentModel|ZeroValueType.NotConfigured=ZeroValueType.NotConfigured
   public extraColumns:ComponentModel[]|ZeroValueType.NotConfigured=ZeroValueType.NotConfigured
   constructor() {
+  }
+  setColumnHeaderComponents(columnHeaderComponents: ComponentModel[]|ZeroValueType.NotConfigured){
+    this.columnHeaderComponents=columnHeaderComponents
+    return this
+  }
+  setFooter(footer: ComponentModel|ZeroValueType.NotConfigured){
+    this.footer=footer
+    return this
+  }
+  setCaption(caption: ComponentModel|ZeroValueType.NotConfigured){
+    this.caption=caption
+    return this
+  }
+  setExtraColumns(extraColumns:ComponentModel[]|ZeroValueType.NotConfigured){
+    this.extraColumns=extraColumns
+    return this
   }
 }
