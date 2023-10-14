6f630555ca3d02e15eaf8b77e2f0c695b023a2d1
Paul Holsters <paul.holsters@hotmail.com>
2023-10-14 11:52:37 +0200

    verwijderen datalink prop uit ClientDataConfigModel.ts

--- src/app/design-dimensions/ClientData/ClientDataConfigModel.ts
+++ src/app/design-dimensions/ClientData/ClientDataConfigModel.ts
@@ -5,7 +5,6 @@ export class ClientDataConfigModel {
   // dit is de data integratie
 constructor(
   public actionId:ActionIdType,
-  public dataLink:string[]|NoValueType.NO_VALUE_ALLOWED=NoValueType.NO_VALUE_ALLOWED,
   public errorMessages:string[]|NoValueType.NO_VALUE_ALLOWED=NoValueType.NO_VALUE_ALLOWED,
   public hardcodedData:any|NoValueType.NO_VALUE_ALLOWED=NoValueType.NO_VALUE_ALLOWED,
   ) {
